0781490c06f9c8220dcd23f64cc61c19c6fcdd9e
NatuRosen <natan.rosenhain@gmail.com>
2024-03-22 10:27:48 -0300

    Carpeta TP 1 y Ej. 1
    Carpeta del TP 1
    Esta el el Ej. 1, todavía no lo probé

---
+++ TP 1/ej_1/ej_1.ino
@@ -0,0 +1,26 @@
+// Inclusion de librerias
+#include <U8g2lib.h>
+
+/* ----------------------------------------------------------------------------------------------------- */
+
+// Definicion de constructores y variables globales
+
+U8G2_SH1106_128X64_NONAME_F_HW_I2C oled(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+/* ----------------------------------------------------------------------------------------------------- */
+
+
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println(F("OLED test"));
+  oled.begin();
+}
+
+void loop() 
+{
+  oled.clearBuffer();
+
+  oled.setFont(u8g2_font_ncenB14_tr);
+  oled.drawStr(15,15, "Hola Mundo");
+}
